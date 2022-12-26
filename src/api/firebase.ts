bde10d08de1ac4ab3f1cd4661b74999c4642c29d
@dlemetin <dlemetin@kameleoon.com>
2022-12-26 11:11:51 +0300

    test 5

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -2,12 +2,12 @@ import firebase, { initializeApp, FirebaseOptions, getApp, } from "firebase/app"
 import { getDownloadURL, getStorage, listAll, ref } from "firebase/storage"
 
 const firebaseConfig: FirebaseOptions = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: 'AIzaSyCJOYuvCt3qmK3JX4zGlqW7DatXPX5vLxQ',
+    authDomain: 'edds-b64b5.firebaseapp.com',
+    projectId: 'edds-b64b5',
+    storageBucket: 'edds-b64b5.appspot.com',
+    messagingSenderId: '30614745257',
+    appId: '1:30614745257:web:f0e20f880bf8a277f3f9b8',
 }
 
 function createFirebaseApp(config: FirebaseOptions) {
